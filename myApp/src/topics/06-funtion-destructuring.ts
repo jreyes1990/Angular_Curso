4049c7b345ac6571a9dee00560604c5da7f680ea
jreyes1990 <jarl1990mt@gmail.com>
2023-09-10 23:21:54 -0600

    Resolución de la tarea - Desestructuración

--- myApp/src/topics/06-funtion-destructuring.ts
+++ myApp/src/topics/06-funtion-destructuring.ts
@@ -18,25 +18,27 @@ interface TaxtCalculationOptions {
   products: Product[];
 }
 
-function textCalculation( options:TaxtCalculationOptions ):number[] {
+//function taxtCalculation( options:TaxtCalculationOptions ): [number, number] {
+//function taxtCalculation( { tax, products }:TaxtCalculationOptions ): [number, number] {
+function taxtCalculation( options:TaxtCalculationOptions ): [number, number] {
+  const { tax, products } = options;
   let total=0;
 
-  options.products.forEach( product => {
-    //total = total + product.price;
-    total += product.price;
+  products.forEach( ({ price }) => {
+    total += price;
   });
 
-  return [ total, total*options.tax ];
+  return [ total, total*tax ];
 }
 
 const shoppingCart = [ phone, tablet ];
 const tax = 0.15;
 
-const result = textCalculation({
+const [ total, totalTax ] = taxtCalculation({
   products: shoppingCart,
   tax: tax,
 });
 
-console.log(`Total: ${result[0]}\nTax: ${result[1]}`)
+console.log(`Total: ${total}\nTax: ${totalTax}`)
 
 export {};
