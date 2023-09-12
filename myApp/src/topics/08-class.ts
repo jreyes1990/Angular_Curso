c32244c1a54089d2f76394c1c641bec4ef2a7f31
jreyes1990 <jarl1990mt@gmail.com>
2023-09-11 23:18:57 -0600

    Constructor de una clase

--- myApp/src/topics/08-class.ts
+++ myApp/src/topics/08-class.ts
@@ -1,13 +1,18 @@
 export class Person {
-  public name:string;
-  private address:string;
+  // public name:string;
+  // private address:string;
 
-  constructor(){
-    this.name = 'Jolberth';
-    this.address = 'New York';
+  // constructor(){
+  //   this.name = 'Jolberth';
+  //   this.address = 'New York';
+  // }
+
+  constructor( public name:string, private address:string){
+    // this.name = name;
+    // this.address = address;
   }
 }
 
-const iroman = new Person();
+const iroman = new Person('Alberto', 'Santa Lucia Cotz.');
 
 console.log(iroman)
