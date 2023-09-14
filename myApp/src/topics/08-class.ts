b23a2c2261c39886b4f6f7b05796d863e479d69a
jreyes1990 <jarl1990mt@gmail.com>
2023-09-13 23:21:40 -0600

    Priorizar composición sobre herencia

--- myApp/src/topics/08-class.ts
+++ myApp/src/topics/08-class.ts
@@ -7,19 +7,27 @@ export class Person {
   //   this.address = 'New York';
   // }
 
-  constructor( public name:string, private address:string){
+  constructor( public firtName:string, public lastName:string, private address?:string){
     // this.name = name;
     // this.address = address;
   }
 }
 
-export class Hero extends Person {
-  constructor( public alterEgo:string, public age:number, public realName:string ){
-    super( realName, 'New York');
+// export class Hero extends Person {
+//   constructor( public alterEgo:string, public age:number, public realName:string ){
+//     super( realName, 'New York');
+//   }
+// }
+
+export class Hero {
+  //public person: Person;
+
+  constructor( public alterEgo:string, public age:number, public realName:string, public person: Person ){
+    // this.person = new Person(realName);
   }
 }
 
-
-const iroman = new Hero('Alberto', 33, 'Jolberth');
+const tony = new Person('Tony', 'Start', 'New York');
+const iroman = new Hero('Alberto', 33, 'Jolberth', tony);
 
 console.log(iroman)
