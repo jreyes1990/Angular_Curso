75a5525c3eebd71cecfbde0e7707726d403ec079
jreyes1990 <jarl1990mt@gmail.com>
2023-09-16 18:42:48 -0600

    Contador

--- bases/src/app/app.component.ts
+++ bases/src/app/app.component.ts
@@ -5,7 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   public title:string = 'Hola mundo, Bienvenido a mi primera App de Angular';
   public descripcion:string = "Footer -> bienvenidos a Angular";
+  public counter:number = 10;
+
+  increaseBy( value:number ):void {
+    if (value === 0) {
+      this.counter = 10;
+    } else {
+      this.counter += value;
+    }
+  }
+
+  resetCounter(){
+    this.counter = 10;
+  }
 }
