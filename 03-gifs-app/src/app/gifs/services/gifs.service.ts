b36150f0b5fc2218514286143015c172aade326b
jreyes1990 <jarl1990mt@gmail.com>
2023-09-24 21:53:49 -0600

    97. Leer del LocalStorage

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -12,7 +12,9 @@ export class GifsService {
   private apiKey:string = '1BRyEKQNJJTCEC5wjl4aOyLgnT3ybwcr';
   private serviceUrl:string = 'https://api.giphy.com/v1/gifs';
 
-  constructor( private http:HttpClient ) { }
+  constructor( private http:HttpClient ) {
+    this.loadLocalStorge();
+  }
 
   get tagsHistory(){
     return [...this._tagsHistory];
@@ -32,6 +34,14 @@ export class GifsService {
 
   private saveLocalStorage():void {
     localStorage.setItem('history', JSON.stringify( this._tagsHistory ) );
+  }
+
+  private loadLocalStorge():void {
+    if( !localStorage.getItem('history') ) return;
+    this._tagsHistory = JSON.parse( localStorage.getItem('history')! );
+
+    if( this._tagsHistory.length === 0 ) return;
+    this.searchTag( this._tagsHistory[0] );
   }
 
   searchTag( tag:string ):void {
