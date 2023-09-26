072d18e8e13b693997d633f202c9107a28aa172a
jreyes1990 <jarl1990mt@gmail.com>
2023-09-25 21:47:40 -0600

    103. Solución de la tarea

--- 03-gifs-app/src/app/gifs/gifs.module.ts
+++ 03-gifs-app/src/app/gifs/gifs.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home/home-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box.components';
 import { CardListComponent } from './components/card-list/card-list.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
     HomePageComponent,
     SearchBoxComponent,
     CardListComponent,
+    CardComponent,
   ],
   imports: [
     CommonModule
